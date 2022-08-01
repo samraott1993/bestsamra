b1baecefc9d76783e55a06d595bb144e64a90a67
samraott1993 <110082983+samraott1993@users.noreply.github.com>
2022-08-01 16:58:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,28 +1,28 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a> HEY M U R FRIEND ADD ME TO YOUR GROUP 😍"""
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, HEY SEXY ADD KRO MUJE APNE GROUP ME 😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/samraott1>samra</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/samraott1>SAMRA</a>
+✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: <a href=https://t.me/samraott1>PROGRAM</a>
+✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: <a href=https://t.me/samraott1>𝙿𝚈𝚃𝙷𝙾𝙽 𝟹</a>
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]
-✯ JOIN MY CHANNEL https://t.me/samraott1"""
+✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: <a href=https://t.me/samraott1>𝙷𝙴𝚁𝙾𝙺𝚄</a>
+✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: <a href=https://t.me/samraott1>v1.0.1 [ 𝙱𝙴𝚃𝙰 ]</a>
+✯ JOIN MY CHANNEL ( https://t.me/samraott1 )"""
     SOURCE_TXT = """<b>NOTE:</b>
-- MY NAME IS SHEFFYSAMRA. 
-- Source - https://t.me/samraott1  
+- KAUR PREET is a CLOSED source project. 
+- Source - CLOSED  
 
 <b>DEVS:</b>
-- <a href=https://t.me/sheffysamra>SHEFFYSAMRA</a>"""
+- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. KAUR B should have admin privillage.
+1. KAUR PREET should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -33,7 +33,7 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- KAUR B Supports both url and alert inline buttons.
+- Eva Maria Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
@@ -41,7 +41,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/samraott1_bot)</code>
+<code>[Button Text](buttonurl:https://t.me/Sharedpost123bot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
